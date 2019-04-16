ea21d3ecf139a25ef13841fef8d5d70121c78afa
Jesus <jlobec93@gmail.com>
2019-04-16 13:18:00 +0200

    Adding floating action button to add new poll

--- decisionsystem-front/src/components/Decision/MyDecisions.js
+++ decisionsystem-front/src/components/Decision/MyDecisions.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { withStyles } from "@material-ui/core";
+import List from "@material-ui/core/List";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
 import axios from "axios";
 import Poll from "./Poll";
 
@@ -39,17 +42,37 @@ class MyDecisions extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
     const openPolls = this.state.polls.map((poll, index) => {
       return <Poll key={index} poll={poll} />;
     });
     return (
       <React.Fragment>
-        <div>{openPolls}</div>
+        <List>{openPolls}</List>
+        <Fab color="secondary" aria-label="Add" className={classes.fab}>
+          <AddIcon />
+        </Fab>
       </React.Fragment>
     );
   }
 }
 
-const styles = theme => ({});
+const styles = theme => ({
+  fab: {
+    position: "absolute",
+    [theme.breakpoints.down("sm")]: {
+      bottom: theme.spacing.unit * 3,
+      right: theme.spacing.unit * 3
+    },
+    [theme.breakpoints.up("md")]: {
+      bottom: theme.spacing.unit * 3,
+      right: theme.spacing.unit * 3
+    },
+    [theme.breakpoints.up("lg")]: {
+      bottom: theme.spacing.unit * 6,
+      right: theme.spacing.unit * 6
+    }
+  }
+});
 
 export default withStyles(styles)(MyDecisions);
