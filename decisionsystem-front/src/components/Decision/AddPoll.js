b2c98c8d750e716755b3ca906967743ec0cc30e5
Jesus <jlobec93@gmail.com>
2019-04-17 13:01:17 +0200

    Start/end poll time with material-ui-picker

--- decisionsystem-front/src/components/Decision/AddPoll.js
+++ decisionsystem-front/src/components/Decision/AddPoll.js
@@ -14,10 +14,25 @@ import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  TimePicker,
+  DatePicker
+} from "material-ui-pickers";
 import axios from "axios";
-import withMobileDialog from "@material-ui/core/withMobileDialog";
 
 import { config } from "../../config";
+
+const getStartDefaultValue = () => {
+  return new Date();
+};
+
+const getEndDefaultValue = () => {
+  let now = new Date();
+  now.setDate(now.getDate() + 14);
+  return now;
+};
 
 const initialState = {
   open: false,
@@ -25,17 +40,18 @@ const initialState = {
   assemblyOptions: [],
   title: "",
   description: "",
+  startTime: "",
+  endTime: "",
   pollType: "",
   assemblyId: 0,
-  pollOptions: [{ name: "name", description: "desc" }]
+  pollOptions: [{ name: "", description: "" }]
 };
+
+initialState.startTime = getStartDefaultValue();
+initialState.endTime = getEndDefaultValue();
 
 class AddPoll extends React.Component {
   state = { ...initialState };
-
-  getStartDefaultValue = () => {
-    //2017-05-24T10:30
-  };
 
   getAuth = () => {
     const token = sessionStorage.getItem("jwtToken");
@@ -100,6 +116,14 @@ class AddPoll extends React.Component {
     });
   };
 
+  handleStartTimeChange = date => {
+    this.setState({ startTime: date });
+  };
+
+  handleEndTimeChange = date => {
+    this.setState({ endTime: date });
+  };
+
   handlePollOptions = classes => {
     let drawRemoveBtn = this.state.pollOptions.length > 1;
     const optionInputs = this.state.pollOptions.map((option, index) => {
@@ -109,8 +133,9 @@ class AddPoll extends React.Component {
       drawRemoveBtn =
         drawRemoveBtn && index !== this.state.pollOptions.length - 1;
       return (
-        <ListItem>
+        <ListItem key={`${index}${now}`}>
           <TextField
+            required
             autoFocus
             margin="dense"
             id={`${now}${option.name}`}
@@ -187,12 +212,13 @@ class AddPoll extends React.Component {
     const pollToCreate = {
       title: this.state.title,
       description: this.state.description,
-      startTime: "",
-      endTime: "",
+      startTime: this.state.startTime,
+      endTime: this.state.endTime,
       pollType: this.state.pollType,
       assemblyId: this.state.assemblyId,
       options: this.state.pollOptions
     };
+    console.log(pollToCreate);
     // let successful = false;
     // const { data: userFound } = await this.getUser(this.state.usernameOrEmail);
     // console.log("find user result");
@@ -223,114 +249,124 @@ class AddPoll extends React.Component {
     const pollOptions = this.handlePollOptions(classes);
     return (
       <div>
-        <Dialog
-          fullScreen={this.state.fullScreen}
-          open={this.state.open}
-          onClose={this.handleClose}
-          aria-labelledby="form-dialog-title"
-        >
-          <DialogTitle id="form-dialog-title">New Poll</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Please enter new poll information.
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="title"
-              name="title"
-              label="Poll title"
-              type="text"
-              value={this.state.title}
-              onChange={this.handleChange}
-              fullWidth
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="description"
-              name="description"
-              label="Description"
-              type="text"
-              value={this.state.description}
-              onChange={this.handleChange}
-              fullWidth
-            />
-            <TextField
-              id="startTime"
-              name="startTime"
-              label="Starts at"
-              type="datetime-local"
-              className={classes.dateTimePicker}
-              //   defaultValue={new Date()}
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
-            <TextField
-              id="endTime"
-              name="endTime"
-              label="Closes at"
-              type="datetime-local"
-              className={classes.dateTimePicker}
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
-            <FormControl
-              variant="outlined"
-              className={classes.formControl}
-              fullWidth
-            >
-              <InputLabel>Poll type</InputLabel>
-              <Select
-                native
-                value={this.state.pollType}
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Dialog
+            fullScreen={this.state.fullScreen}
+            open={this.state.open}
+            onClose={this.handleClose}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogTitle id="form-dialog-title">New Poll</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Please enter new poll information.
+              </DialogContentText>
+              <TextField
+                required
+                autoFocus
+                margin="dense"
+                id="title"
+                name="title"
+                label="Poll title"
+                type="text"
+                value={this.state.title}
                 onChange={this.handleChange}
-                inputProps={{
-                  name: "pollType",
-                  id: "pollType"
-                }}
-              >
-                <option value="" />
-                <option value={"simple"}>Single option</option>
-              </Select>
-            </FormControl>
-            <FormControl
-              variant="outlined"
-              className={classes.formControl}
-              fullWidth
-            >
-              <InputLabel>Assembly</InputLabel>
-              <Select
-                native
-                value={this.state.assemblyId}
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="description"
+                name="description"
+                label="Description"
+                type="text"
+                value={this.state.description}
                 onChange={this.handleChange}
-                inputProps={{
-                  name: "assemblyId",
-                  id: "assemblyId"
-                }}
+                fullWidth
+              />
+              <DatePicker
+                name="startTime"
+                margin="normal"
+                label="Start date"
+                value={this.state.startTime}
+                onChange={this.handleStartTimeChange}
+              />
+              <TimePicker
+                name="startTime"
+                margin="normal"
+                label="Start hour"
+                value={this.state.startTime}
+                onChange={this.handleStartTimeChange}
+              />
+              <DatePicker
+                name="endTime"
+                margin="normal"
+                label="End date"
+                value={this.state.endTime}
+                onChange={this.handleEndTimeChange}
+              />
+              <TimePicker
+                name="endTime"
+                margin="normal"
+                label="End hour"
+                value={this.state.endTime}
+                onChange={this.handleEndTimeChange}
+              />
+              <FormControl
+                variant="outlined"
+                className={classes.formControl}
+                fullWidth
               >
-                <option value={0} />
-                {assemblyOptions}
-              </Select>
-            </FormControl>
+                <InputLabel>Poll type</InputLabel>
+                <Select
+                  native
+                  value={this.state.pollType}
+                  onChange={this.handleChange}
+                  inputProps={{
+                    name: "pollType",
+                    id: "pollType"
+                  }}
+                >
+                  <option value="" />
+                  <option value={"simple"}>Single option</option>
+                </Select>
+              </FormControl>
+              <FormControl
+                variant="outlined"
+                className={classes.formControl}
+                fullWidth
+              >
+                <InputLabel>Assembly</InputLabel>
+                <Select
+                  native
+                  value={this.state.assemblyId}
+                  onChange={this.handleChange}
+                  inputProps={{
+                    name: "assemblyId",
+                    id: "assemblyId"
+                  }}
+                >
+                  <option value={0} />
+                  {assemblyOptions}
+                </Select>
+              </FormControl>
 
-            {/* <Divider variant="middle" /> */}
-            <Typography gutterBottom variant="body1">
-              Poll options
-            </Typography>
-            {pollOptions}
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={this.handleCreatePoll} color="primary">
-              Create
-            </Button>
-          </DialogActions>
-        </Dialog>
+              {/* <Divider variant="middle" /> */}
+              <Typography gutterBottom variant="body1">
+                Poll options
+              </Typography>
+              {pollOptions}
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={this.handleCreatePoll} color="primary">
+                Create
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </MuiPickersUtilsProvider>
       </div>
     );
   }
