c6bd42c329d2298ff4fc69290b9d15b7691ab621
jlobec <jlobec@centolo>
2019-05-12 20:57:06 +0200

    First version of score voting poll option component

--- decisionsystem-front/src/components/Decision/PollOption.js
+++ decisionsystem-front/src/components/Decision/PollOption.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { withStyles } from "@material-ui/core";
+import { withStyles, Typography } from "@material-ui/core";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
-import IconButton from "@material-ui/core/IconButton";
-import CommentIcon from "@material-ui/icons/Comment";
-import axios from "axios";
-
-import { config } from "../../config";
+import Slider, { defaultValueReducer } from "@material-ui/lab/Slider";
+import Grid from "@material-ui/core/Grid";
+import Tooltip from "@material-ui/core/Tooltip";
+import Badge from "@material-ui/core/Badge";
+import Chip from "@material-ui/core/Chip";
 
 const initialState = {
-  checked: []
+  checked: [],
+  value: 1
 };
 
 class PollOption extends React.Component {
@@ -19,6 +19,10 @@ class PollOption extends React.Component {
 
   handleToggle = value => () => {
     this.props.handleSelectOption(value);
+  };
+
+  handleScoreChange = (event, value) => {
+    this.setState({ value });
   };
 
   async componentDidMount() {}
@@ -58,15 +62,31 @@ class PollOption extends React.Component {
   renderScoreSystem(classes, pollOption, checked) {
     return (
       <React.Fragment>
-        <ListItem
-          role={undefined}
-          dense
-          button
-          onClick={this.handleToggle(pollOption.pollOptionId)}
-        >
-          <Checkbox checked={checked} tabIndex={-1} disableRipple />
-          <ListItemText primary={`${pollOption.name} `} />
-          <ListItemText secondary={`${pollOption.description}`} />
+        <ListItem>
+          <Grid container direction="column" spacing={8}>
+            <Grid item xs>
+              <ListItemText primary={`${pollOption.name} `} />
+              <ListItemText secondary={`${pollOption.description}`} />
+            </Grid>
+            <Grid item xs>
+              <Slider
+                classes={{ container: classes.slider }}
+                value={this.state.value}
+                min={1}
+                max={5}
+                step={1}
+                onChange={this.handleScoreChange}
+                thumb={
+                  // <Badge badgeContent={this.state.value} color="secondary" />
+                  <Chip
+                    className={classes.chip}
+                    label={`${this.state.value}`}
+                    color="secondary"
+                  />
+                }
+              />
+            </Grid>
+          </Grid>
         </ListItem>
       </React.Fragment>
     );
@@ -78,6 +98,13 @@ class PollOption extends React.Component {
   }
 }
 
-const styles = theme => ({});
+const styles = theme => ({
+  slider: {
+    padding: "22px 0px"
+  },
+  chip: {
+    padding: "10px"
+  }
+});
 
 export default withStyles(styles)(PollOption);
